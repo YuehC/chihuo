5fa04f3e6e2df5c6243e93f17a727c73a306ac48
nhuang <388580@qq.com>
2017-03-28 18:12:04 +0800

    see contact

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -7,6 +7,13 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+     appPages=[
+    { title: '我的课表', component: '', icon: 'ios-book-outline',iconColor:'#ff3366' },
+    { title: '机构管理', component: '', icon: 'ios-medal-outline',iconColor:'#10aeff' },
+    { title: '我的账户', component: '', icon: 'ios-happy-outline',iconColor:'#ffc701' },
+    { title: '我的钱包', component:'', icon: 'md-mail-open',iconColor:'#10aeff'}
+  ];
+
 
   constructor(public navCtrl: NavController) {
 
